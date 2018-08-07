197878c50332d4f02eab6460f73db590e01d008c
Jayesh Dhamsaniya <jayeshdhamsaniya@gmail.com>
2018-08-07 15:15:44 +0530

    = regular update

---
+++ BYTESCOUT DOCUMENT PARSER SDK/C#/GeneralExample/GeneralExample/Program.cs
@@ -0,0 +1,64 @@
+﻿//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up //
+//                                                                                           //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 //
+// http://www.bytescout.com                                                                  //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using ByteScout.DocumentParser;
+
+// This example demonstrates document data parsing to JSON and YAML formats.
+
+namespace GeneralExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDocument1 = @".\DigitalOcean.pdf";
+            string inputDocument2 = @".\AmazonAWS.pdf";
+ 
+            // Create DocumentParser instance
+            using (DocumentParser documentParser = new DocumentParser("demo", "demo"))
+            {
+                Console.WriteLine("Loading templates...");
+                 
+                int count = documentParser.AddTemplates(templatesFolder);
+ 
+                Console.WriteLine("{count} templates loaded.");
+                Console.WriteLine();
+ 
+                Console.WriteLine("Parsing \"{inputDocument1}\"...");
+                Console.WriteLine();
+ 
+                // Parse document data in JSON format
+                string jsonString = documentParser.ParseDocument(inputDocument1, OutputFormat.JSON);
+                // Display parsed data in console
+                Console.WriteLine("Parsing results in JSON format:");
+                Console.WriteLine();
+                Console.WriteLine(jsonString);
+ 
+                Console.WriteLine();
+                Console.WriteLine("Parsing \"{inputDocument2}\"...");
+                Console.WriteLine();
+ 
+                // Parse document data in YAML format
+                string yamlString = documentParser.ParseDocument(inputDocument2, OutputFormat.YAML);
+                // Display parsed data in console
+                Console.WriteLine("Parsing results in YAML format:");
+                Console.WriteLine();
+                Console.WriteLine(yamlString);
+            }
+ 
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
